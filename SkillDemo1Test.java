8f4bc3c000de39ddc6b91e53447f3c518ee2ef3a
Alexander-Qinn <97425867+Alexander-Qinn@users.noreply.github.com>
2022-02-04 01:15:16 -0800

    Fixing error in tester

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test
     public void testMult(){
-        assertEquals(12, SkillDemo1.mult(2,3));
+        assertEquals(6, SkillDemo1.mult(2,3));
     }
 }
